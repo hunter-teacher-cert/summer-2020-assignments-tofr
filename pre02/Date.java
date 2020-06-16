970969e92b3c2ddd2691b3511a804ca3176b147e
tofr <mykolyk@stuycs.org>
2020-06-16 18:45:21 -0400

    2

---
+++ pre02/Date.java
@@ -0,0 +1,41 @@
+
+public class Date {
+
+  public static void main( String[] args ) {
+
+    //String firstLine = "Hello, again!";
+    //System.out.println(firstLine);
+
+    String day = "Tuesday";
+    System.out.println(day);
+
+    int date = 16;
+    System.out.println(date);
+
+    String month = "June";
+    System.out.println(month);
+
+    int year = 2020;
+    System.out.println();
+
+
+
+    //When we talk about displaying a variable, we generally mean the value of the variable. To display the name of a variable, you have to put it in quotes.
+    //System.out.print("The value of firstLine is ");
+    //System.out.println(firstLine);
+
+    //    For this example, the output is:
+    //The value of firstLine is Hello, again!
+
+    //Conveniently, the syntax for displaying a variable is the same regardless of its type. For example:
+    int hour = 11;
+    int minute = 59;
+    System.out.print("The current time is ");
+    System.out.print(hour);
+    System.out.print(":");
+    System.out.print(minute);
+    System.out.println(".");
+
+  }//end main
+
+}//end class
