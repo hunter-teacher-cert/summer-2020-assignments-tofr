6cf4886130c449283a8b5d3a4a6b8174107f9d27
tofr <mykolyk@stuycs.org>
2020-06-18 19:53:40 -0400

    +date  time cel->far

--- pre02/Date.java
+++ pre02/Date.java
@@ -7,34 +7,38 @@ public class Date {
     //System.out.println(firstLine);
 
     String day = "Tuesday";
-    System.out.println(day);
+    //System.out.println(day);
 
     int date = 16;
-    System.out.println(date);
+    //System.out.println(date);
 
     String month = "June";
-    System.out.println(month);
+    //System.out.println(month);
 
     int year = 2020;
+    //System.out.println(year);
+
+    //amurrkan style
+    System.out.println("American format");
+    System.out.print(day);
+    System.out.print(", ");
+    System.out.print(month);
+    System.out.print(" ");
+    System.out.print(date);
+    System.out.print(", ");
+    System.out.print(year);
+
     System.out.println();
 
-
-
-    //When we talk about displaying a variable, we generally mean the value of the variable. To display the name of a variable, you have to put it in quotes.
-    //System.out.print("The value of firstLine is ");
-    //System.out.println(firstLine);
-
-    //    For this example, the output is:
-    //The value of firstLine is Hello, again!
-
-    //Conveniently, the syntax for displaying a variable is the same regardless of its type. For example:
-    int hour = 11;
-    int minute = 59;
-    System.out.print("The current time is ");
-    System.out.print(hour);
-    System.out.print(":");
-    System.out.print(minute);
-    System.out.println(".");
+    //euro style
+    System.out.println("European format");
+    System.out.print(day);
+    System.out.print(" ");
+    System.out.print(date);
+    System.out.print(" ");
+    System.out.print(month);
+    System.out.print(" ");
+    System.out.print(year);
 
   }//end main
 
---
+++ pre02/Exer02.java
@@ -0,0 +1,44 @@
+/**
+   https://books.trinket.io/thinkjava/chapter3.html
+   3.12
+   Exercise 2
+   Write a program that converts a temperature from Celsius to Fahrenheit.
+   It should
+   (1) prompt the user for input,
+   (2) read a double value from the keyboard,
+   (3) calculate the result, and
+   (4) format the output to one decimal place.
+   For example, it should display "24.0 C = 75.2 F".
+   Here is the formula. Be careful not to use integer division!
+   F = C * 9/5 + 32
+**/
+import java.util.Scanner;
+
+public class Exer02 {
+
+  public static void main( String[] args ) {
+
+    Scanner in = new Scanner(System.in);
+
+    // (1) prompt the user for input,
+    System.out.println(" tempC:");
+
+    // (2) read a double value from the keyboard,
+    double inTempC;
+    inTempC = in.nextDouble();
+
+    // (3) calculate the result, and
+    double outTempF;
+    outTempF = inTempC * 9/5. + 32;
+
+    // (4) format the output to one decimal place.
+    System.out.printf(" %.1f C",inTempC);
+    System.out.print(" = ");
+    System.out.printf("%.1f F",outTempF);
+    System.out.println();
+
+    // For example, it should display "24.0 C = 75.2 F".
+
+  }
+
+}
