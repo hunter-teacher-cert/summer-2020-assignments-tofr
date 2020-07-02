aeb741fac4c70161e1166ea8ddc10db0be8edfe6
tofr <mykolyk@stuycs.org>
2020-07-02 13:35:19 -0400

    grow Time.java

--- classwork/03class/Driver.java
+++ classwork/03class/Driver.java
@@ -1,8 +1,47 @@
+/**
+   classwork -- Day 3
+   intro to classes & objects
+   M. Zamansky, T. Mykolyk, F. Dragon
+   June 2020
+**/
+
 import java.io.*;
 import java.util.*;
 
-public class Driver{
-    public static void main(String[] args) {
-	Time t = new Time();
-    }
-}
+public class Driver
+{
+  public static void main( String[] args ) {
+
+    //invoke default constructor
+    Time t1 = new Time();
+    System.out.println(t1);
+    //System.out.printf(t1);
+
+    //invoke overloaded constructor
+    Time t2 = new Time(11, 59, 59.9);
+    System.out.println(t2);
+    //System.out.printf(t2);
+    /*
+      System.out.println(t2.hour);
+      ...will generate this error:
+      error: hour has private access in Time
+    System.out.println(t2.hour);
+    */
+
+    Time time1 = new Time(9, 30, 0.0);
+    Time time2 = time1;
+    Time time3 = new Time(9, 30, 0.0);
+
+    System.out.println( time1.equals(time3) );
+
+    Time startTime = new Time(18, 50, 0.0);
+    Time runningTime = new Time(2, 16, 0.0);
+    Time endTime = Time.add(startTime, runningTime);
+
+    System.out.println(startTime);
+    System.out.println(runningTime);
+    System.out.println(endTime);
+
+  }
+
+}//end Driver
--- classwork/03class/Time.java
+++ classwork/03class/Time.java
@@ -1,9 +1,160 @@
+/**
+   classwork -- Day 3
+   intro to classes & objects
+   https://books.trinket.io/thinkjava/chapter11.html
+
+   M. Zamansky, T. Mykolyk, F. Dragon
+   June 2020
+**/
+
 import java.io.*;
 import java.util.*;
 
+public class Time
+{
+  //instance variables
+  private int hour;
+  private int minute;
+  private double second;
 
-public class Time{
+  //default constructor
+  public Time()
+  {
+    //explicit:
+    // this.hour = 0;
+    // this.minute = 0;
+    // this.second = 0.0;
 
-    // your time class code goes in here
-    
-}
+    hour = 0;
+    minute = 0;
+    second = 0.0;
+  }
+
+  //overloaded constructor
+  public Time(int hour, int minute, double second)
+  {
+    this.hour = hour;
+    this.minute = minute;
+    this.second = second;
+  }
+
+  
+  //"accessors" aka "getters"
+  public int getHour()
+  {
+    return this.hour;
+  }
+
+  public int getMinute()
+  {
+    return this.minute;
+  }
+
+  public double getSecond()
+  {
+    return this.second;
+  }
+
+  
+  //"mutators" aka "setters"
+  public void setHour(int hour)
+  {
+    this.hour = hour;
+  }
+
+  public void setMinute(int minute)
+  {
+    this.minute = minute;
+  }
+
+  public void setSecond(int second)
+  {
+    this.second = second;
+  }
+
+  /*  
+  public static void printTime(Time t)
+  {
+    System.out.print(t.hour);
+    System.out.print(":");
+    System.out.println(t.minute);
+    System.out.print(":");
+    System.out.println(t.second);
+  }
+  */
+
+  public static void printTime(Time t) {
+    System.out.printf("%02d:%02d:%04.1f\n",
+                      t.hour, t.minute, t.second);
+  }
+
+
+  //overload inherited toString()
+  public String toString()
+  {
+    return String.format("%02d:%02d:%04.1f\n",
+                         this.hour, this.minute, this.second);
+  }
+
+  //overload inherited equals()
+  public boolean equals(Time that)
+  {
+    return this.hour == that.hour
+      && this.minute == that.minute
+      && this.second == that.second;
+  }
+
+
+  public static Time add(Time t1, Time t2)
+  {
+    Time sum = new Time();
+    sum.hour = t1.hour + t2.hour;
+    sum.minute = t1.minute + t2.minute;
+    sum.second = t1.second + t2.second;
+    return sum;
+  }
+
+  /* naive version
+  public Time add(Time t2)
+  {
+    Time sum = new Time();
+    sum.hour = this.hour + t2.hour;
+    sum.minute = this.minute + t2.minute;
+    sum.second = this.second + t2.second;
+    return sum;
+  }
+  */
+  
+  //improved add() -- now actually adds! (handles carrying)
+  public Time add(Time t2)
+  {
+    Time sum = new Time();
+    sum.hour = this.hour + t2.hour;
+    sum.minute = this.minute + t2.minute;
+    sum.second = this.second + t2.second;
+
+    if (sum.second >= 60.0) {
+      sum.second -= 60.0;
+      sum.minute += 1;
+    }
+    if (sum.minute >= 60) {
+      sum.minute -= 60;
+      sum.hour += 1;
+    }
+    return sum;
+  }
+
+
+  public void increment(double seconds)
+  {
+    this.second += seconds;
+    while (this.second >= 60.0) {
+      this.second -= 60.0;
+      this.minute += 1;
+    }
+    while (this.minute >= 60) {
+      this.minute -= 60;
+      this.hour += 1;
+    }
+  }  
+}//Time
