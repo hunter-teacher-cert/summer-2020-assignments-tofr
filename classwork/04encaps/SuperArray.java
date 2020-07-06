7ffb542a2aad3a79e801ef252d12550c1d46c5f9
tofr <mykolyk@stuycs.org>
2020-07-06 14:09:41 -0400

    + methods implemented

--- classwork/04encaps/SuperArray.java
+++ classwork/04encaps/SuperArray.java
@@ -23,45 +23,71 @@ public class SuperArray
   {
     // test to see if we need to grow, then grow
 
+    if (numberElements == data.length){
+      grow();
+    }
 
     // add item
+    data[numberElements] = value;
+    numberElements = numberElements + 1;
+
   }
 
-  public void add(int index, int value){
+  public void add(int index, int value)
+  {
     // see if there's enough room
+    if ( numberElements == data.length ){
+      grow();
+    }
 
     // shift the items down
+    for( int i=numberElements; i > index ; i--) { //traverse R-to-L. (Why?)
+      data[i]=data[i-1];
+    }
 
     // insert the item
+    data[index] = value;
 
-    // increment numElements
-
+    // increment numberElements
+    numberElements++ ;
   }
 
-  public void remove(int index){
 
+  public void remove(int index)
+  {
     // shift items down to remove the item at index
-
+    for( int i=index; i<numberElements-1; i++ ){
+      data[i] = data[i+1];
+    }
     // subtract fom numElements;
-
+    numberElements--;
   }
 
-  public boolean isEmpty(){
+
+  public boolean isEmpty()
+  {
     return numberElements == 0;
   }
 
-  public int get(int index){
+
+  public int get(int index)
+  {
     return data[index];
   }
 
-  public String toString(){
+
+  public String toString()
+  {
     String s = "";
     for (int i = 0; i < numberElements; i++) {
       s = s + data[i] + ", ";
     }
     return s;
   }
-  public String debug(){
+
+
+  public String debug()
+  {
     String s = "";
     s = "Size: " + this.data.length;
     s = s + " LastItem: " + numberElements + "  Data: ";
@@ -72,12 +98,21 @@ public class SuperArray
     return s;
   }
 
-  private void grow(){
+
+  private void grow()
+  {
     // create a new array with extra space
+    int newSize = data.length + (data.length / 2);
+    int[] newData = new int[newSize];
 
     // copy over all the elements from the old array to the new one
+    for( int i=0; i<numberElements; i++ ){
+      newData[i] = data[i];
+    }
 
     // point data to the new array
+    data = newData;
 
-  }
-}
+  }//end grow()
+
+}//end class
